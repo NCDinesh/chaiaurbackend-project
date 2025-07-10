904c018a37055904b32f665bab753f8ffbb7b9d4
NCDinesh <deenez.npl1@gmail.com>
2025-07-10 14:13:59 +0545

    Publish a video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,6 +1,6 @@
 import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
+import {Video} from "../models/video.models.js"
+import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -13,8 +13,56 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
+    const { title, description, isPublished = true} = req.body
+
+    if(!title || title?.trim()==="") {
+        throw new ApiError(400, "Title Content is required")
+    }
+
+    if(!description || description?.trim()=== "") {
+        throw new ApiError(400, "Description content is required")
+    }
+
+    const videoFileLocalPath = req.files?.videoFile?.[0].path
+    const thumbnailFileLocalPath = req.files?.thumbnail?.[0].path
+
+    if(!videoFileLocalPath) {
+        throw new ApiError(400, "Video File missing!")
+    }
+
+    const videoFile = await uploadOnCloudinary(videoFileLocalPath)
+    const thumbnail = await uploadOnCloudinary(thumbnailFileLocalPath)
+
+    if(!videoFile) {
+        throw new ApiError(500, "Something went wrong while uploading video file on cloudinary")
+    } 
+
+    const video = await Video.create({
+        videoFile : {
+            public_id : videoFile?.public_id,
+            url: videoFile?.url
+        },
+
+        thumbnail: {
+            public_id : thumbnail?.public_id,
+            url: thumbnail?.url
+        },
+        title,
+        description,
+        isPublished,
+        owner: req.user._id,
+        duration: videoFile?.duration
+    })
+
+    if(!video) {
+        throw new ApiError(500, "Something went wrong while storing the video in database")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(200,video, "Video uploaded successfully")
+    )
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,46 +1,62 @@
-import mongoose , {Schema} from "mongoose"
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({
-    videoFile : {
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      public_id: {
         type: String,
-        required:true
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
     },
-    thumbnail : {
-        type:String,
-        required:true
+    thumbnail: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
     },
 
     title: {
-        type:String,
-        required:true
+      type: String,
+      required: true,
     },
 
-    description : {
-        type:String,
-        required:true
+    description: {
+      type: String,
+      required: true,
     },
 
-    duration : {
-        type:Number,
-        required:true
+    duration: {
+      type: Number,
+      required: true,
     },
 
     views: {
-        type:Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
 
     isPublished: {
-        type:Boolean,
-        default:true},
-
-        owner: {
-        type:Schema.Types.ObjectId,
-        ref: "User"
+      type: Boolean,
+      default: true,
     },
-},{timestamps:true})
 
-videoSchema.plugin(mongooseAggregatePaginate)
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Video = mongoose.model("Video", videoSchema)
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
