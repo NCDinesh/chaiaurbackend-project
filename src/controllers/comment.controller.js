09ba4c13743ff387e00138acd40ffb592fb3a1cf
NCDinesh <deenez.npl1@gmail.com>
2025-07-15 08:17:52 +0545

    Update comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -41,7 +41,44 @@ const addComment = asyncHandler(async (req, res) => {
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+   const {newContent} = req.body
+   const {commentId} = req.params
+
+   if(!newContent || newContent.trim()===""){
+    throw new ApiError(400, "This video id is not valid")
+   }
+
+   const comment = await Comment.findById(commentId)
+
+   if(!comment) {
+    throw new ApiError(404, "Comment not found")
+   }
+
+   if(comment.Owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You don't have permission to update this comment")
+   }
+
+   const updateComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+        $set: {
+            content:newContent
+        }
+    },
+
+    {
+        new:true
+    }
+   )
+
+   if(!updateComment) {
+    throw new ApiError(400, "Error while updating comment")
+   }
+
+   return res.status(200)
+   .json(
+    new ApiResponse(200,updateComment,"comment updated successfully")
+   )
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-import { addComment } from "../controllers/comment.controller.js"
+import { addComment, updateComment } from "../controllers/comment.controller.js"
 
 const router=Router()
 
 router.route("/addcomment/:videoId").post(verifyJWT,addComment)
+router.route("/updatecomment/:commentId").post(verifyJWT,updateComment)
 
 export default router
