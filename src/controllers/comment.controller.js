4d8e259f03be60be65b67ee6b7a963d7b7c2a5ff
NCDinesh <deenez.npl1@gmail.com>
2025-07-15 09:36:44 +0545

    Comment controller completed

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,15 +1,73 @@
 import mongoose, { isValidObjectId } from "mongoose"
 import {Comment} from "../models/comment.models.js"
+import {Video} from "../models/video.models.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
-})
+  if (!mongoose.Types.ObjectId.isValid(videoId)) {
+  throw new ApiError(400, "This video id is not valid");
+}
+
+
+    const video = await Video.findById(videoId)
+    if(!video) {
+        throw new ApiError (404,"Video not found")
+    }
+
+    const aggregateComments = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+
+        {
+            $sort: {
+                createdAt : -1
+            }
+        },
+
+        {
+            $lookup: {
+                from: "users",
+                localField:"owner",
+                foreignField:"_id",
+                as: "owner"
+            }
+        },
+
+       { $addFields: {
+            owner : {$first: "$owner"}
+        }},
+
+        {
+            $project: {
+                content: 1,
+                createdAt:1,
+                updatedAt:1,
+                "owner.fullName":1,
+                "owner.username":1,
+                "owner.avatar":1
+            }
+        }
+    ])
+
+    const result = await Comment.aggregatePaginate(aggregateComments, {
+    page: parseInt(page),
+    limit: parseInt(limit),
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result, "Video comments fetched successfully"));
+});
 
 const addComment = asyncHandler(async (req, res) => {
     const {comment} = req.body
@@ -82,7 +140,32 @@ const updateComment = asyncHandler(async (req, res) => {
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    const {commentId} = req.params
+
+    if(!commentId){
+        throw new ApiError(404,"This comment id is not valid")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if(!comment){
+        throw new ApiError(404,"Comment not found")
+    }
+
+    if(comment.Owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You don't have permission to delete this comment")
+    }
+
+    const deleteComment= await Comment.deleteOne({_id:commentId})
+
+    if(!deleteComment){
+        throw new ApiError(500, "something went wrong while deleting comment")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200, deleteComment, "Comment deleted successfully")
+    )
 })
 
 export {
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,10 +1,12 @@
 import {Router} from "express"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-import { addComment, updateComment } from "../controllers/comment.controller.js"
+import { addComment, deleteComment, getVideoComments, updateComment } from "../controllers/comment.controller.js"
 
 const router=Router()
 
 router.route("/addcomment/:videoId").post(verifyJWT,addComment)
 router.route("/updatecomment/:commentId").post(verifyJWT,updateComment)
+router.route("/deletecomment/:commentId").post(verifyJWT,deleteComment)
+router.route("/getcomment/:videoId").get(verifyJWT,getVideoComments)
 
 export default router
